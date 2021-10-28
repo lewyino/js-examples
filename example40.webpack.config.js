6893ce758c9900503a38ad00d41af3ee83b927a9
lewyino <mateusz.lewandowski@gmail.com>
2021-10-28 23:02:21 +0200

    add babel to webpack

--- example40.webpack.config.js
+++ example40.webpack.config.js
@@ -7,5 +7,23 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/examples/es6/40_modules'),
         filename: 'app.js',
     },
-    plugins: [new HtmlWebpackPlugin({ template: './examples/es6/40_modules/modules.html' })],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './examples/es6/40_modules/modules.html',
+            filename: "module.html"
+        })
+    ],
 };
